7b4e689786c45066145ff12a84ae942056654b47
F0xedb <tom@odex.be>
2020-03-16 18:44:21 +0200

    Happy pi day!

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type piIndex struct {
+	Index       int
+	Input       string
+	Surrounding string
+}
+
+type piResult struct {
+	Index       int
+	Surrounding string
+}
+
+var piDigits string
+
+var speed = 100000
+
+func main() {
+	http.HandleFunc("/search/", SearchServer)
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", fs)
+	loadFile("pi-million.txt")
+	http.ListenAndServe(":8080", nil)
+}
+
+// streamline type checks
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func loadFile(filename string) {
+	file, err := os.Open(filename)
+	check(err)
+	buffer := make([]byte, speed)
+	piDigits = ""
+	for {
+		size, err := file.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		check(err)
+		piDigits += string(buffer[:size])
+	}
+	fmt.Println("Pi Digits loaded in!")
+
+}
+
+// Get the index in pi of a given string
+func getIndex(index string) piResult {
+	var position = strings.Index(piDigits[2:], index) + 1
+	if position <= 0 {
+		position = -1
+	}
+	return piResult{position, piDigits[position+1 : position+11]}
+}
+
+// Webserver endpoints are beeing served here
+
+// EchoServer default / endpoint echoing to parameters of the URL back
+func EchoServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Echo: %s", r.URL.Path[1:])
+}
+
+// SearchServer This function is served on /search/<num> it retrieves the index found in pi.
+// SearchServer With a resolution of 1 Billion
+func SearchServer(w http.ResponseWriter, r *http.Request) {
+	var splittedPath = strings.Split(r.URL.Path, "/")
+	var index = splittedPath[len(splittedPath)-1]
+	var result = getIndex(index)
+	piResult := piIndex{result.Index, index, result.Surrounding}
+	js, err := json.Marshal(piResult)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
---
+++ public/index.js
@@ -0,0 +1,46 @@
+function Get(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl, false); // false for synchronous request
+  xmlHttp.send();
+  return JSON.parse(xmlHttp.responseText);
+}
+
+// append an entry to the table
+// input is what the user had given as a option
+// json is the returning value from the rest server
+function appendTable(input, json) {
+  var table = document.getElementById("output");
+  let row = table.insertRow(1);
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  let cell3 = row.insertCell(2);
+  cell1.innerText = input;
+  if (json.Index > 0) {
+    cell2.innerText = json.Index;
+    cell3.innerText = json.Surrounding;
+  } else {
+    cell2.innerText = "Could not find result";
+    cell3.innerText = "No neighbours because no match was found";
+  }
+}
+
+function update() {
+  // getting dom elements
+  let input = document.getElementById("input");
+  let result = document.getElementById("result");
+  // only update if a value was entered
+  if (input.value == "") return;
+  // do the api call
+  let out = Get("http://localhost:8080/search/" + input.value);
+  appendTable(input.value, out);
+}
+
+function clearTable() {
+  var table = document.getElementById("output");
+  console.log(table.rows.length);
+  // length is up here because the table always gets updated thus each time we loop over the for our endpoint is different
+  var length = table.rows.length;
+  for (let i = 1; i < length; i++) {
+    table.deleteRow(-1);
+  }
+}
